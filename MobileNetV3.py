12be0ca20931fe4c94feeef22bb247556586ac91
Will@A.&H <1173510911@qq.com>
2019-06-05 12:32:33 +0800

    Update MobileNetV3.py

--- MobileNetV3.py
+++ MobileNetV3.py
@@ -10,6 +10,7 @@ import numpy as np
 from TensorFlow_Utils import MobileNetV3_small as mnv3s_model
 import cv2
 import random
+import copy
 
 def parse_line(line):
 	s = line.strip().split(' ')
@@ -36,8 +37,8 @@ def parse_data(file_path):
 
 def divide_data(data_list):
 	random.shuffle(data_list)
-	test_list = data_list[:5000]
-	train_list = data_list[5000:]
+	test_list = data_list[:1000]
+	train_list = data_list[1000:]
 	return train_list, test_list
 
 def consume_data(img_lst, batch_size):
@@ -49,44 +50,79 @@ def consume_data(img_lst, batch_size):
 		labels.append(label)
 	return images, labels
 
-def evaluate(pred_list, anno_list):
-	# pred_list: np.array
-	# anno_list: char list
+def convert_to_one_hot(labels):
+	labels_len = len(labels)
+	labels_one_hot = np.zeros([labels_len, 2])
+	for idx in range(0, labels_len):
+		if labels[idx]=='0':
+			labels_one_hot[idx][0] = 1
+		elif labels[idx]=='1':
+			labels_one_hot[idx][1] = 1
+	return labels_one_hot 
+
+def evaluate_acc(pred_list, anno_list):
+	# pred_list: np.array, "prediction list"
+	# anno_list: char list, "annotation list"
 	anno_list = np.array(anno_list)
 	anno_list = anno_list.astype(np.int64)
-	difference = anno_list - pred_list
-	difference_abs = np.absolute(difference)
-	difference_sum = np.sum(difference_abs)
-	return difference_sum/5000.
+	tp_count = 0
+	fp_count = 0
+	fn_count = 0
+	tn_count = 0
+	for idx in range(0, len(anno_list)):
+		if ((anno_list[idx]==1) and (pred_list[idx]==1)):
+			tp_count = tp_count + 1
+		elif ((anno_list[idx]==0) and (pred_list[idx]==1)):
+			fp_count = fp_count + 1
+		elif ((anno_list[idx]==1) and (pred_list[idx]==0)):
+			fn_count = fn_count + 1
+		elif ((anno_list[idx]==0) and (pred_list[idx]==0)):
+			tn_count = tn_count + 1
+	precision =  tp_count / (tp_count + fp_count)
+	recall = tp_count / (tp_count + fn_count)
+	info = "precision: {:.3f}, recall: {:.3f}".format(precision, recall)
+	print(info)
+	return
 
-if __name__ == '__main__':
-
-	image_list = parse_data('cuhkpq.txt')
+def train():
+	image_list = parse_data('cuhkpq_train.txt')
+	random.shuffle(image_list)
 	train_list, test_list = divide_data(image_list)
-	batch_size = 16
+	train_list_backup = copy.deepcopy(train_list)
+	test_list_backup = copy.deepcopy(test_list)
+	batch_size = 64
 	train_len = len(train_list)
 
+	save_dir = './checkpoints/'
 
 	# Single GPU Version
 	input_images = tf.placeholder(shape=[None, 224, 224, 3], dtype=tf.float32)
-	input_labels = tf.placeholder(shape=[None, 1], dtype=tf.int32)
+	input_labels = tf.placeholder(shape=[None, 2], dtype=tf.float32)
 
 	pred = mnv3s_model(input_images)
-	loss = tf.losses.sparse_softmax_cross_entropy(labels=input_labels, logit=pred)
 
-	optimizer = tf.train.Adam(0.001)
-	train_op = optimizer.minimize(loss)
+	loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=input_labels, logits=pred))
+
+	update_vars = tf.contrib.framework.get_variables_to_restore()
+
+	optimizer = tf.train.AdamOptimizer(0.0003)
+	grads = optimizer.compute_gradients(loss, var_list=update_vars)
+	train_op = optimizer.apply_gradients(grads)
+
+	saver_to_save = tf.train.Saver()
 
 	init = tf.global_variables_initializer()
 
-	epoches = 100
+	epoches = 5000
 	global_step = 1
 
 	with tf.Session() as sess:
 		sess.run(init)
 		for i in range(1, epoches+1):
+			train_list = copy.deepcopy(train_list_backup)
 			for j in range(0, int(train_len/batch_size)):
 				images, labels = consume_data(train_list, batch_size)
+				labels = convert_to_one_hot(labels)
 				_, pred_, loss_ = sess.run([train_op, pred, loss], feed_dict={input_images:images, input_labels:labels})
 				
 				info = "Epoch: {}, global_step: {}, loss: {:.3f}".format(i, global_step, loss_)
@@ -95,16 +131,55 @@ if __name__ == '__main__':
 				if (global_step%200 == 0):
 					pred_lst = []
 					anno_lst = []
-					for m in range(0, 500):
+					test_list = copy.deepcopy(test_list_backup)
+					for m in range(0, 100):
 						images, labels = consume_data(test_list, 10)
 						predictions = sess.run([pred], feed_dict={input_images:images})
 						predictions = np.argmax(predictions[0], axis=1)
 						pred_lst.extend(predictions)
 						anno_lst.extend(labels)
-					accuracy = evaluate(pred_lst, anno_lst)
-					info = "global_step: {}, accuracy: {:.3f}".format(global_step, accuracy)
-					print(info)
+					evaluate_acc(pred_lst, anno_lst)
+
+				if (global_step%500 == 0):
+					saver_to_save.save(sess, save_dir+'model-step_{}_loss_{:4f}'.format(global_step, loss_))
 
 				global_step = global_step + 1
 
+def eval_pretrained():
+	# parse .txt file
+	image_list = parse_data('cuhkpq_test.txt')
+	random.shuffle(image_list)
+	test_list = image_list[0:5000]
+	ckpt_dir = './checkpoints/'
+	batch_size = 10
 
+	# Single GPU Version
+	input_images = tf.placeholder(shape=[None, 224, 224, 3], dtype=tf.float32)
+	input_labels = tf.placeholder(shape=[None], dtype=tf.int32)
+
+	pred = mnv3s_model(input_images)
+	
+	saver_to_restore = tf.train.Saver()
+
+	init = tf.global_variables_initializer()
+
+	with tf.Session() as sess:
+		sess.run(init)
+		saver_to_restore.restore(sess, './checkpoints/mnv3.ckpt')
+
+		pred_lst = []
+		anno_lst = []
+		#test_list = image_list
+		for m in range(0, len(test_list)//batch_size):
+			images, labels = consume_data(test_list, batch_size)
+			predictions = sess.run([pred], feed_dict={input_images:images})
+			predictions = np.argmax(predictions[0], axis=1)
+			pred_lst.extend(predictions)
+			anno_lst.extend(labels)
+		evaluate_acc(pred_lst, anno_lst)
+				
+if __name__ == '__main__':
+	train()
+	# eval_pretrained()
+
+	
